9afe46bb24f64ee2c60e486becbb0c70c5e2ff3d
Pawan Paudel <pawanpaudel93@gmail.com>
2023-06-13 22:02:58 +0545

    feat: Add filter, sort and reset operations

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,49 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [fileName, setFileName] = useState("");
   const [format, setFormat] = useState("m3u");
-  const parser = new M3uParser();
+  const [filterOption, setFilterOption] = useState<{
+    key: string;
+    filters: string[];
+    retrieve: string;
+    nestedKey: string;
+  }>({
+    key: "name",
+    filters: [],
+    retrieve: "true",
+    nestedKey: "false",
+  });
+
+  const [sortOption, setSortOption] = useState<{
+    key: string;
+    asc: string;
+    nestedKey: string;
+  }>({
+    key: "name",
+    asc: "true",
+    nestedKey: "false",
+  });
+
+  const keys = [
+    "name",
+    "logo",
+    "url",
+    "category",
+    "tvg-id",
+    "tvg-name",
+    "tvg-url",
+    "live",
+    "country-code",
+    "country-name",
+    "language-code",
+    "language-name",
+  ];
+  const [parser] = useState(new M3uParser());
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "audio/mpegurl": [".m3u"],
     },
     onDrop: async (acceptedFiles) => {
-      // Handle dropped files and render JSON
       setIsLoading(true);
       const file = acceptedFiles[0];
       setFileName(file.name.split(".").slice(0, -1).join("."));
@@ -29,9 +64,38 @@ const App = () => {
     await parser.saveToFile(fileName, format);
   }
 
+  async function filter() {
+    setIsLoading(true);
+    const { key, nestedKey, filters, retrieve } = filterOption;
+    parser.filterBy(
+      key,
+      filters,
+      retrieve === "true",
+      nestedKey === "true",
+      "-"
+    );
+    setRenderedJSON(parser.getStreamsInfo());
+    setIsLoading(false);
+  }
+
+  async function sort() {
+    setIsLoading(true);
+    const { key, nestedKey, asc } = sortOption;
+    parser.sortBy(key, asc === "true", nestedKey === "true", "-");
+    setRenderedJSON(parser.getStreamsInfo());
+    setIsLoading(false);
+  }
+
+  async function resetOperations() {
+    setIsLoading(true);
+    parser.resetOperations();
+    setRenderedJSON(parser.getStreamsInfo());
+    setIsLoading(false);
+  }
+
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 bg-gray-200 p-8">
+    <div className="flex flex-col sm:flex-row h-screen">
+      <div className="w-full sm:w-1/2 bg-gray-200 p-8">
         <div
           {...getRootProps({
             className:
@@ -51,8 +115,106 @@ const App = () => {
             <div className="spinner border-t-2 border-b-2 border-gray-400 rounded-full w-8 h-8 animate-spin"></div>
           </div>
         )}
+        {/* Add filter and sort options here */}
+        <div className="flex flex-col my-4">
+          <div className="flex items-center mb-4">
+            <span className="mr-2 text-black">Filter by:</span>
+            <select
+              className="border border-gray-300 rounded px-2 py-1"
+              value={filterOption.key}
+              onChange={(event) =>
+                setFilterOption((prevOption) => ({
+                  ...prevOption,
+                  key: event.target.value,
+                  nestedKey:
+                    event.target.value.indexOf("-") > -1 ? "true" : "false",
+                }))
+              }
+            >
+              <option value="">Select Key</option>
+              {keys.map((key) => (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              ))}
+            </select>
+            <input
+              type="text"
+              className="border border-gray-300 rounded px-2 py-1 ml-2 w-full sm:w-96"
+              placeholder="Filter values separated by comma"
+              value={filterOption.filters.join(",")}
+              onChange={(event) =>
+                setFilterOption((prevOption) => ({
+                  ...prevOption,
+                  filters: event.target.value.split(","),
+                }))
+              }
+            />
+            <select
+              className="border border-gray-300 rounded px-2 py-1 ml-2"
+              value={filterOption.retrieve}
+              onChange={(event) =>
+                setFilterOption((prevOption) => ({
+                  ...prevOption,
+                  retrieve: event.target.value,
+                }))
+              }
+            >
+              <option value="true">Retrieve</option>
+              <option value="false">Exclude</option>
+            </select>
+
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded ml-2"
+              onClick={filter}
+            >
+              Filter
+            </button>
+          </div>
+          <div className="flex items-center mb-4">
+            <span className="mr-2 text-black">Sort by:</span>
+            <select
+              className="border border-gray-300 rounded px-2 py-1"
+              value={sortOption.key}
+              onChange={(event) =>
+                setSortOption((prevOption) => ({
+                  ...prevOption,
+                  key: event.target.value,
+                  nestedKey:
+                    event.target.value.indexOf("-") > -1 ? "true" : "false",
+                }))
+              }
+            >
+              <option value="">Select Key</option>
+              {keys.map((key) => (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              ))}
+            </select>
+            <select
+              className="border border-gray-300 rounded px-2 py-1 ml-2"
+              value={sortOption.asc}
+              onChange={(event) =>
+                setSortOption((prevOption) => ({
+                  ...prevOption,
+                  asc: event.target.value,
+                }))
+              }
+            >
+              <option value="true">Ascending</option>
+              <option value="false">Descending</option>
+            </select>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded ml-2"
+              onClick={sort}
+            >
+              Sort
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="w-1/2 bg-gray-100 p-8 overflow-auto">
+      <div className="w-full sm:w-1/2 bg-gray-100 p-8 overflow-auto h-full">
         {!isLoading && Object.keys(renderedJSON).length > 0 && (
           <div className="flex items-center mb-4 sticky top-0 bg-white p-4 z-50">
             <span className="mr-2 text-black">Download Format:</span>
@@ -77,6 +239,13 @@ const App = () => {
                 }}
               >
                 Clear
+              </button>
+
+              <button
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-3 rounded"
+                onClick={resetOperations}
+              >
+                Reset Operations
               </button>
             </div>
           </div>
